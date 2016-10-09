381fc60b73e61271b84dbdbd204e5bab5b943bdc
Leboucetmistere <arthurdp96@hotmail.fr>
2016-10-09 22:50:36 +0200

    Commit 2

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "sqr.h"
+ 
+int main()
+{
+	auto f=sqr(9);
+	std::cout<<f<<std::endl;
+  	return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+#include "sqr.h"
+
+TEST_CASE( "SQRT are computed", "SQRT" ) {
+    REQUIRE( sqr(1) == 1 );
+    REQUIRE( sqr(4) == 16 );
+    REQUIRE( sqr(3) == 10 );
+}
