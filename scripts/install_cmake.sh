405e4c2322b34737d790faa59924852a45342150
Leboucetmistere <arthurdp96@hotmail.fr>
2016-10-15 23:17:01 +0200

    try script

---
+++ scripts/install_cmake.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz
+tar -xvf cmake-3.3.2-Linux-x86_64.tar.gz > /dev/null
+mv cmake-3.3.2-Linux-x86_64 cmake-install
